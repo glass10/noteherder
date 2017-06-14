9a840f692f264a1550fa7de17f2730e03159fc4b
Cameron Glass <glass10@purdue.edu>
2017-06-14 17:22:37 -0400

    Finish css files for all components

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import './NoteForm.css'
 
 class NoteForm extends Component{
     render(){
         return(
-            <p>NOTEFORM!</p>
+            <div className="NoteForm">
+                <form>
+                <p>
+                    <input type="text" name="title" placeholder="Title your note" value="" />
+                </p>
+                <p>
+                    <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+                </p>
+                </form>
+            </div>
         )
     }
 }
