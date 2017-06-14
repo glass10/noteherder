e58ead8d1052baa4fb388bc593b6bebb62d4e760
Cameron Glass <glass10@purdue.edu>
2017-06-14 16:29:54 -0400

    Load images in script

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import quill from './quill.svg'
+import newHover from './new-hover.png'
+import newIcon from './new.png'
 
 const Sidebar = () =>{
     return(
-        <p>SIDEBAR!</p>
+        <nav className="Sidebar">
+        <div className="logo">
+          <img src={quill} alt="Noteherder" />
+        </div>
+        <button className="new-note">
+          <img src={newHover} alt="New note" />
+          <img className="outline" src={newIcon} alt="New note" />
+        </button>
+      </nav>
     )
 }
 
