4d3c7078df32da93d15fdb41ecd9aeeafe42f413
Cameron Glass <glass10@purdue.edu>
2017-06-15 14:59:51 -0400

    Store database in localStorage

--- src/NoteList.js
+++ src/NoteList.js
@@ -2,20 +2,11 @@ import React, { Component } from 'react'
 import './NoteList.css'
 import NoteForm from './NoteForm'
 
-const testNotes = [
-    {
-        title: "Citizens of distant epochs",
-        body: "Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are softly dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?"
-    },
-    {
-        title: "Preserve and cherish that pale blue dot ",
-        body: "network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!"
-    },
-    {
-        title: "Laws of physics",
-        body: "Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are softly dancing galaxies inconspicuous motes of rock and gas"
-    }
-]
+
+
+const testNotes = JSON.parse(localStorage.getItem("notes"));
+
+
 let self = this;
 
 class NoteList extends Component{
@@ -56,6 +47,7 @@ class NoteList extends Component{
     }
 
     updateNotes(){
+        localStorage.setItem("notes", JSON.stringify(testNotes));
         const state = {...this.state};
         state.notes = testNotes;
         this.setState(state);
@@ -64,8 +56,8 @@ class NoteList extends Component{
     render(){
         return(
             <div className="NoteList">
+                {this.updateNotes}
                 <h3>Notes</h3>
-                {this.loopThrough}
                 <ul id="notes">
                     {testNotes.map((note, i) => <NoteItem note={note} key = {i}/>)}
                 </ul>
