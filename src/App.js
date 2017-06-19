5ce6d279758ce2f9009db80e53a1cf04fd130bb5
Cameron Glass <glass10@purdue.edu>
2017-06-19 14:18:48 -0400

    Add Signed In and Signed Out components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './Main'
+import base from './base'
+import NoteList from './NoteList'
+import SignIn from './SignIn'
+import SignOut from './SignOut'
 
 class App extends Component {
+
+  signedIn = () =>{
+    return true;
+  }
+
+  renderMain = () =>{
+    return(
+      <div>
+        <SignOut />
+        <Main />
+      </div>
+    )
+
+  }
   render() {
     return (
       <div className="App">
-        <Main />
+        {this.signedIn ? this.renderMain() : <SignIn />}
       </div>
+    )
+  }
+
+  componentWillMount(){
+    base.syncState(
+      'notes',
+      {
+        context: this,
+        state: 'NoteList.state.notes'
+      }
     );
   }
 }
