46540d7946dc5a56566bed22b43f3cc88bd607be
Cameron Glass <glass10@purdue.edu>
2017-06-20 14:47:32 -0400

    Style login buttons to match icons

--- src/SignIn.js
+++ src/SignIn.js
@@ -11,16 +11,18 @@ const SignIn = () => {
   return (
     <div>
       <button
-        className="SignIn"
+        className="SignIn GitHub"
         onClick={() => authenticate(githubProvider)}
       >
+      <img alt="" src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"></img>
         Sign In With GitHub
       </button>
       <button
-        className="SignIn"
+        className="SignIn Google"
         onClick={() => authenticate(googleProvider)}
       >
-        Sign In With Google
+      <img alt="" src="http://diaryofasocialgal.com/wp-content/uploads/2016/10/google-plus-icon.png"></img>
+      Sign In With Google
       </button>
     </div>
   )
