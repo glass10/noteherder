fb2b3e366ffd49084b9730029f5cde2b804bb03f
Cameron Glass <glass10@purdue.edu>
2017-06-21 18:22:52 -0400

    Add Rich Text Editor. Gets body but not editable

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
+import RichTextEditor from 'react-rte';
+import PropTypes from 'prop-types'; // ES6
 
 import './NoteForm.css'
 
 class NoteForm extends Component {
+  static propTypes = {
+    onChange: PropTypes.func
+  };
+
+  state = {
+    value: RichTextEditor.createValueFromString(this.props.currentNote.body, 'html')
+  }
+
   componentWillReceiveProps(nextProps) {
     const newId = nextProps.match.params.id
 
@@ -23,6 +33,18 @@ class NoteForm extends Component {
   handleRemove = (ev) => {
     this.props.removeNote(this.props.currentNote)
   }
+
+  onChange = (value) => {
+    this.setState({value});
+    if (this.props.onChange) {
+      // Send the changes up to the parent component as an HTML string.
+      // This is here to demonstrate using `.toString()` but in a real app it
+      // would be better to avoid generating a string on each change.
+      this.props.onChange(
+        value.toString('html')
+      );
+    }
+  };
 
   render() {
     return (
@@ -45,6 +67,13 @@ class NoteForm extends Component {
               value={this.props.currentNote.body}
             ></textarea>
           </p>
+          <div>
+          {/*<Editor editorState={this.props.currentNote.body} onChange={this.handleChanges} />*/}
+          <RichTextEditor
+            value={RichTextEditor.createValueFromString(this.props.currentNote.body, 'html')}
+            onChange={this.onChange}
+          />
+          </div>
           <button
            type="button"
            onClick={this.handleRemove}
