aaa1033bda8d8e237e95123024cd32d1242bcc57
Cameron Glass <glass10@purdue.edu>
2017-06-21 14:24:11 -0400

    Add React Router

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+ReactDOM.render(
+    <Router>
+        <Route component={ App } />
+    </Router>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
